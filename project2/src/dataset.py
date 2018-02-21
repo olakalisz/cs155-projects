7d0e59226466cc1f5bc9a43d81ab92457091aba1
Riley Patterson <rpatt@caltech.edu>
2018-02-21 14:55:20 -0800

    dataset: fix movie caching

--- project2/src/dataset.py
+++ project2/src/dataset.py
@@ -1,4 +1,3 @@
-from cached_property import cached_property
 import numpy as np
 
 # movie metadata encoded as indicated in MOVIE_FEATURES
@@ -31,24 +30,29 @@ MOVIE_FEATURES = [
 ]
 
 class Movie:
+    # class property to store dataset once
+    _movies = {}
+
     def __init__(self, id):
         self.id = id
-        raw_data = self.movies[id]
+        raw_data = self.movies()[id]
         self.title = raw_data[0]
         self.genres = []
         for genre, indicator in zip(MOVIE_FEATURES[3:], raw_data[2:]):
             if int(indicator):
                 self.genres.append(genre)
 
-    @cached_property
-    def movies(self):
+    @classmethod
+    def movies(cls):
         """A dictionary of all movie data.
 
         The dictionary is keyed on integer movie ids and contains string values mapping to
         MOVIE_FEATURES defined above.
 
         """
-        return {
-            int(r[0]): r[1:]
-            for r in np.loadtxt(MOVIES, dtype=str, delimiter='\t', encoding='latin1')
-        }
+        if not cls._movies:
+            cls._movies = {
+                int(r[0]): r[1:]
+                for r in np.loadtxt(MOVIES, dtype=str, delimiter='\t', encoding='latin1')
+            }
+        return cls._movies
