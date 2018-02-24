a04129993d4697c5eba41b07dce1957c9d5281d3
Riley Patterson <rpatt@caltech.edu>
2018-02-23 19:39:58 -0800

    visualize_svd: factor out code for producing six plots so that it can be applied to each method cleanly
    * also split the genre plots into three by default

--- project2/src/visualize_svd.py
+++ project2/src/visualize_svd.py
@@ -74,15 +74,84 @@ def visualize_2d(M, index=None, labels=[], color=lambda id: 'black', alpha=1,
     else:
         plt.show()
 
-    return M_proj
+
+def produce_plots(V, method_name, ratings_all, M, N, imagebasename=None, singlegenreplot=False):
+    """Produce the six plots specified in the homework for the given V matrix.
+
+    Specifically, produces plots of the following to either files with the given prefix or device:
+        * Action and Romance movies occurring in the top thirty most rated movies
+        * Top ten movies by number of ratings
+        * Top ten movies by average rating
+        * Ten movies each from genres Animation, Drama, Sci-Fi
+
+    """
+    def filename(suffix):
+        return f'{imagebasename}_{method_name}_{suffix}.png' if imagebasename else None
+
+    movie_titles = [dataset.Movie(id).title for id in range(1, N+1)]
+
+    # project action and romance movies in top 30 by frequency of ratings
+    top_thirty_id = list(dataset.top_most_rated_movies(ratings_all, n=30) - np.ones(30, dtype=int))
+    action_romance_id = [m.id for m in dataset.Movie.query(genres=['Romance', 'Action'])]
+    # tranform to zero indexing to match transformed dataset
+    action_romance_id = action_romance_id - np.ones(len(action_romance_id), dtype=int)
+    action_romance_index = list(set(action_romance_id).intersection(set(top_thirty_id)))
+    visualize_2d(
+        V, index=action_romance_index,
+        labels=movie_titles, title=f'{method_name} Projection of Action/Romance Movies',
+        filename=filename('action_romance'))
+
+    # labeling ten outliers
+    visualize_2d(
+        V, label_outliers=10, labels=movie_titles,
+        title=f'{method_name} with ten outliers labeled',
+        filename=filename('outliers'))
+
+    # find the most popular movies
+    top_ten_id = list(dataset.top_most_rated_movies(ratings_all, n=10) - np.ones(10, dtype=int))
+    visualize_2d(
+        V, index=top_ten_id, labels=movie_titles,
+        title=f'{method_name} with ten most rated movies labeled',
+        filename=filename('top_ten_frequent'))
+
+    # find the highest average rated movies
+    top_ten_id = list(dataset.top_avg_rated_movies(ratings_all, n=10) - np.ones(10, dtype=int))
+    visualize_2d(
+        V, index=top_ten_id, labels=movie_titles,
+        title=f'{method_name} with ten highest rated movies labeled',
+        filename=filename('top_ten_rated'))
+
+    # label top ten movies in three different genres
+    if singlegenreplot:
+        genre_movie_id = []
+        movie_colors = {} # map from movie id to color based on genre
+        colors = ['blue', 'yellow', 'red']
+        for genre, color in zip(['Animation', 'Drama', 'Sci-Fi'], colors):
+            new_ids = [m.id for m in dataset.Movie.query(genres=[genre])[:10]]
+            genre_movie_id += new_ids
+            for id in new_ids:
+                movie_colors[id] = color
+        genre_movie_id = list(genre_movie_id - np.ones(len(genre_movie_id), dtype=int))
+        visualize_2d(
+            V, index=genre_movie_id, labels=movie_titles,
+            title=f'{method_name} with ten movies from three different genres',
+            color=lambda id: movie_colors[id],
+            legend=[('Animation', 'Drama', 'Sci-Fi'), colors],
+            filename=filename('all_genres'))
+    else:
+        for genre in ['Animation', 'Drama', 'Sci-Fi']:
+            genre_movie_id = [m.id - 1 for m in dataset.Movie.query(genres=[genre])[:10]]
+            visualize_2d(
+                V, index=genre_movie_id, labels=movie_titles,
+                title=f'{method_name} with ten movies from genre {genre}',
+                filename=filename(genre))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--imagebasename', type=str)
+    parser.add_argument('--singlegenreplot', action='store_true', default=False)
     args = parser.parse_args()
-
-    def filename(suffix):
-        return f'{args.imagebasename}_{suffix}.png' if args.imagebasename else None
 
     # load and prepare data
     # NB: ratings_all stays 1-indexed, while Y_train and Y_test are transformed to be 0-indexed
@@ -96,64 +165,16 @@ if __name__ == '__main__':
     Y_train[:,:2] -= np.ones((Y_train.shape[0], 2), dtype=int)
     Y_test[:,:2] -= np.ones((Y_test.shape[0], 2), dtype=int)
     sparse_matrix = dataset.construct_user_movie_matrix(source=dataset.RATINGS_TRAIN, M=M, N=N)
-    movie_titles = [dataset.Movie(id).title for id in range(1, N+1)]
 
     # setting K=20 as specified in the assignment
     K = 20
     eta = 0.03
     reg = 1
 
-    # select movies that we will visualize in our projections: action and romance movies in top 30
-    # by frequency of ratings
-    movie_hist = np.histogram(Y_train[:,1], bins=range(1, M+2))[0]
-    top_id = [id for _, id in sorted(zip(movie_hist, range(1, M+1)), reverse=True)[:30]]
-    action_romance_id = [m.id for m in dataset.Movie.query(genres=['Romance', 'Action'])]
-    # tranform to zero indexing to match transformed dataset
-    action_romance_id = action_romance_id - np.ones(len(action_romance_id), dtype=int)
-    action_romance_index = list(set(action_romance_id).intersection(set(top_id)))
-
     # visualize SVD as implemented for CS155 HW5
     U, V, _ = svd_sgd.train_model(M, N, K, eta, reg, Y_train, max_epochs=300)
-    # visualize movies in 2d
-    visualize_2d(
-        V.transpose(), index=action_romance_index,
-        labels=movie_titles, title='HW5 Implementation 2D Projection',
-        filename=filename('2d_movies_hw5'))
+    produce_plots(V.transpose(), 'HW5', ratings_all, M, N, args.imagebasename, args.singlegenreplot)
 
     # "off-the-shelf" SVD from numpy
     U, V = off_the_shelf.scipy_svd_train(M, N, K, Y_train)
-    visualize_2d(
-        V, label_outliers=10, labels=movie_titles,
-        title='SciPy SVD with ten outliers labeled',
-        filename=filename('2d_outliers_scipy'))
-
-    # find the most popular movies
-    top_ten_id = list(dataset.top_most_rated_movies(ratings_all, n=10) - np.ones(10, dtype=int))
-    visualize_2d(
-        V, index=top_ten_id, labels=movie_titles,
-        title='SciPy SVD with ten most rated movies labeled',
-        filename=filename('2d_top_ten_frequent_scipy'))
-
-    # find the highest average rated movies
-    top_ten_id = list(dataset.top_avg_rated_movies(ratings_all, n=10) - np.ones(10, dtype=int))
-    visualize_2d(
-        V, index=top_ten_id, labels=movie_titles,
-        title='SciPy SVD with ten highest rated movies labeled',
-        filename=filename('2d_top_ten_rated_scipy'))
-
-    # label top ten movies in three different genres
-    genre_movie_id = []
-    movie_colors = {} # map from movie id to color based on genre
-    colors = ['blue', 'yellow', 'red']
-    for genre, color in zip(['Animation', 'Drama', 'Sci-Fi'], colors):
-        new_ids = [m.id for m in dataset.Movie.query(genres=[genre])[:10]]
-        genre_movie_id += new_ids
-        for id in new_ids:
-            movie_colors[id] = color
-    genre_movie_id = list(genre_movie_id - np.ones(len(genre_movie_id), dtype=int))
-    visualize_2d(
-        V, index=genre_movie_id, labels=movie_titles,
-        title='SciPy SVD with ten movies from three different genres',
-        color=lambda id: movie_colors[id],
-        legend=[('Animation', 'Drama', 'Sci-Fi'), colors],
-        filename=filename('2d_genres_scipy'))
+    produce_plots(V, 'SciPy', ratings_all, M, N, args.imagebasename, args.singlegenreplot)
