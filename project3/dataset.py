fd7db8e80026976027f75b6850995f18878e18e2
Riley Patterson <rpatt@caltech.edu>
2018-03-12 21:10:02 -0700

    stress: move basic stuff to dataset, and encode/use for hmm

--- project3/dataset.py
+++ project3/dataset.py
@@ -5,6 +5,7 @@ Common code to load sonnet and syllable data for use in multiple models.
 """
 from collections import defaultdict
 
+PUNCTUATION = "!'(),.:;?\n"
 
 def load_sonnets():
     """Load normalized sonnets into a single list.
@@ -67,3 +68,43 @@ def load_syllables():
             word_syllable_dict[tokens[0]] = syllable_dict[i]
 
     return words, syllable_dict, word_syllable_dict, rev_syllable_dict, rev_end_syllable_dict
+
+
+def stress_repr(sonnets, word_syllables):
+    """Generate the stress representation for the given sonnets.
+
+    For each line of words, turn it into a list of tuples:
+        (first_syllable_stress, word)
+
+    Additionally, produce a stress mapping for each word:
+        TODO
+
+    """
+    encoded_sonnets = []
+    for s in sonnets:
+        encoded_s = []
+        for ln in s:
+            words = [w.strip(PUNCTUATION) for w in ln.split(' ')]
+            if not all([w in word_syllables for w in words]):
+                # skip lines with invalid words. note this is probably a processing problem...
+                continue
+            line_trees = [[(0, words[0], 0)]]
+            for w in words[1:]:
+                new_line_trees = []
+                for t in line_trees:
+                    for syl_count in word_syllables[t[-1][1]]:
+                        new_t = t + [((t[-1][0] + syl_count) % 2, w, t[-1][2] + syl_count)]
+                        new_line_trees.append(new_t)
+                line_trees = new_line_trees
+
+            # find the trees that will make this line exactly 10 syllables
+            for syl_count in word_syllables[w]:
+                culled_line_trees = [[(stress, word) for stress, word, syl_count in tree] for tree in line_trees if tree[-1][2] == 10 - syl_count]
+            if culled_line_trees:
+                encoded_s.append(culled_line_trees[0])
+            else:
+                # didn't find any 10-line candidates. rare so just fall back to taking the first tree
+                encoded_s.append([(stress, word) for stress, word, _ in line_trees[0]])
+        encoded_sonnets.append(encoded_s)
+
+    return encoded_sonnets
